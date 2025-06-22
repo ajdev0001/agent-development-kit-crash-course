76151f99cc2f153c4bc3cd74d032d9631960df46
ajdev0001 <160270486+ajdev0001@users.noreply.github.com>
2025-06-22 16:00:18 +0530

    Use async and await in basic_stateful_session.py
    InMemorySessionService() is an asynchronous function and using async and await keywords will be helpful for better and efficient workflow of the functions.

--- 5-sessions-and-state/basic_stateful_session.py
+++ 5-sessions-and-state/basic_stateful_session.py
@@ -1,65 +1,90 @@
 import uuid
 
 from dotenv import load_dotenv
+import os
 from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
 from google.genai import types
-from question_answering_agent import question_answering_agent
+from google import genai
+from question_answering_agent.agent import Question_Answering_Agent
+import asyncio
 
-load_dotenv()
+load_dotenv('question_answering_agent/.env')
+
+api_key = os.getenv("GOOGLE_API_KEY")
+if api_key:
+    os.environ["GOOGLE_API_KEY"] = api_key
+else:
+    raise RuntimeError("GOOGLE_API_KEY not found in .env file")
+
+#creating instance of agent to pass to the loader
+
+
+initial_state = {
+        "user_name": "Brandon Hancock",
+        "user_preferences": """
+            I like to play Pickleball, Disc Golf, and Tennis.
+            My favorite food is Mexican.
+            My favorite TV show is Game of Thrones.
+            Loves it when people like and subscribe to his YouTube channel.
+        """,
+    }
 
 
 # Create a new session service to store state
-session_service_stateful = InMemorySessionService()
 
-initial_state = {
-    "user_name": "Brandon Hancock",
-    "user_preferences": """
-        I like to play Pickleball, Disc Golf, and Tennis.
-        My favorite food is Mexican.
-        My favorite TV show is Game of Thrones.
-        Loves it when people like and subscribe to his YouTube channel.
-    """,
-}
 
-# Create a NEW session
-APP_NAME = "Brandon Bot"
-USER_ID = "brandon_hancock"
-SESSION_ID = str(uuid.uuid4())
-stateful_session = session_service_stateful.create_session(
-    app_name=APP_NAME,
-    user_id=USER_ID,
-    session_id=SESSION_ID,
-    state=initial_state,
-)
-print("CREATED NEW SESSION:")
-print(f"\tSession ID: {SESSION_ID}")
+    
+async def main():
+    session_service_stateful = InMemorySessionService()
 
-runner = Runner(
-    agent=question_answering_agent,
-    app_name=APP_NAME,
-    session_service=session_service_stateful,
-)
 
-new_message = types.Content(
-    role="user", parts=[types.Part(text="What is Brandon's favorite TV show?")]
-)
+    # Create a NEW session
+    APP_NAME = "Brandon Bot"
+    USER_ID = "brandon_hancock"
+    SESSION_ID = str(uuid.uuid4())
+    stateful_session = await session_service_stateful.create_session(
+        app_name=APP_NAME,
+        user_id=USER_ID,
+        session_id=SESSION_ID,
+        state=initial_state,
+    )
+    print("CREATED NEW SESSION:")
+    print(f"\tSession ID: {SESSION_ID}")
 
-for event in runner.run(
-    user_id=USER_ID,
-    session_id=SESSION_ID,
-    new_message=new_message,
-):
-    if event.is_final_response():
-        if event.content and event.content.parts:
-            print(f"Final Response: {event.content.parts[0].text}")
+    agent_instance = Question_Answering_Agent(initial_state["user_name"], initial_state["user_preferences"])
 
-print("==== Session Event Exploration ====")
-session = session_service_stateful.get_session(
-    app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID
-)
+    runner =  Runner(
+        agent=agent_instance,
+        app_name=APP_NAME,
+        session_service=session_service_stateful,
+    )
 
-# Log final Session state
-print("=== Final Session State ===")
-for key, value in session.state.items():
-    print(f"{key}: {value}")
+    new_message = types.Content(
+        role="user", parts=[types.Part(text="What is Brandon's favorite TV show?")]
+    )
+    print("==== Running Agent ====")
+    for event in runner.run(
+        user_id=USER_ID,
+        session_id=SESSION_ID,
+        new_message=new_message,
+    ):
+            if event.is_final_response():
+                if event.content and event.content.parts:
+                    print(f"Final Response: {event.content.parts[0].text}")
+
+    print("==== Session Event Exploration ====")
+    session = await session_service_stateful.get_session(
+        app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID
+    )
+    # Log all messages in the session
+    print("=== Session Messages ===")
+
+    # Log final Session state
+    print("=== Final Session State ===")
+    for key, value in session.state.items():
+        print(f"{key}: {value}")
+
+
+
+asyncio.run(main())
